5e561933f65ba0cba009fdc76a60c13202f3c126
Victor <vitopcg@gmail.com>
2022-03-03 14:45:01 +0000

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyles />
-    </div>
+    </>
   );
 }
