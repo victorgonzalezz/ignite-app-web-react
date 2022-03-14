84eb886af02ab367e88b2e126e59da9e38f8ec94
Victor <vitopcg@gmail.com>
2022-03-14 15:51:34 +0000

    formatando valores

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,31 @@ import { App } from "./App";
 
 createServer({
   models: {
-    transactions: Model,
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-04-02 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2022-04-08 11:00:00'),
+        }
+
+      ],
+    })
   },
 
   routes() {
